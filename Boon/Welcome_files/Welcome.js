40023f680ef21550a768df0492ddcda196b18372
Richard Hightower <richardhightower@gmail.com>
2013-10-30 08:55:55 -0700

    PIXELS!

--- Boon/Welcome_files/Welcome.js
+++ Boon/Welcome_files/Welcome.js
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Welcome_files/WelcomeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');IMpreload('Welcome_files','shapeimage_3','0');IMpreload('Welcome_files','shapeimage_4','0');IMpreload('Welcome_files','shapeimage_5','0');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');IMpreload('Welcome_files','shapeimage_3','0');IMpreload('Welcome_files','shapeimage_4','0');IMpreload('Welcome_files','shapeimage_5','0');IMpreload('Welcome_files','shapeimage_7','0');IMpreload('Welcome_files','shapeimage_7','1');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
